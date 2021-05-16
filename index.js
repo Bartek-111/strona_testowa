8d5cd26626b23821bab3e8cf88c54e9da44faea8
Bartek-111 <gromulskibartek@gmail.com>
2021-05-16 14:21:08 +0200

    test2

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 console.log("hello world")
-let a = { cd: "hello 2"} 
-console.log(a)
+let ania = { age: 12 };
+let basia = { age: 22 };
+let maciek = { age: 18 };
+function checkIsLegalToDrink(person){
+    // jezeli wiecej lub rowno niz 18 napisz legalne
+    // jezeli nie to napisz niestety brakuje ci jeszcze x lat
+    if(person.age >= 18){
+        console.log("legalne")
+    }
+    if(person.age < 18)
+    {
+        let diffrence = 18 - person.age;
+        console.log(`napisz niestety brakuje ci jeszcze ${diffrence} lat`)
+    }
+}
+console.log("basia")
+checkIsLegalToDrink(basia);
+console.log("ania")
+checkIsLegalToDrink(ania);
+console.log("maciek")
+checkIsLegalToDrink(maciek);
